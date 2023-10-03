e8df3fe739fcc89009ef05e597a66de862a40d9d
shipmaal <shipmaal@bc.edu>
2023-10-03 18:35:55 -0400

    redoing this repo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -32,3 +32,14 @@ if (typeof Worker !== 'undefined') {
   // Web Workers are not supported in this environment.
   // You should add a fallback so that your program still executes correctly.
 }
+if (typeof Worker !== 'undefined') {
+  // Create a new
+  const worker = new Worker(new URL('./app.worker', import.meta.url));
+  worker.onmessage = ({ data }) => {
+    console.log(`page got message: ${data}`);
+  };
+  worker.postMessage('hello');
+} else {
+  // Web Workers are not supported in this environment.
+  // You should add a fallback so that your program still executes correctly.
+}
