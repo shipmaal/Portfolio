bcedfba4afa24e33df081cad8b5b81ffda495906
shipmaal <shipmaal@bc.edu>
2023-10-09 14:44:59 -0400

    idk whats going on

---
+++ firebase.config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCvz5B9HA-KBnyXs1hPODi6EbMbOrnB70Q",
+    authDomain: "portfolio-ea11e.firebaseapp.com",
+    projectId: "portfolio-ea11e",
+    storageBucket: "portfolio-ea11e.appspot.com",
+    messagingSenderId: "216145249345",
+    appId: "1:216145249345:web:6ce35c85cb2c466d1072c0",
+    measurementId: "G-H0W62XYK4L"
+};
