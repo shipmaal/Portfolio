e1e58fe006295b9cbbc9555d3ef09262ddbe055b
shipmaal <shipmaal@bc.edu>
2023-11-02 13:58:48 -0400

    made site more presentable for employers

--- src/app/pages/coding/coding.component.ts
+++ src/app/pages/coding/coding.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ContentContainerComponent } from '@templates/content-container/content-container.component';
+
 @Component({
   selector: 'app-coding',
-  standalone: true,
-  imports: [CommonModule],
+    standalone: true,
+    imports: [CommonModule, ContentContainerComponent],
   templateUrl: './coding.component.html',
   styleUrls: ['./coding.component.scss']
 })
--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ContentContainerComponent } from '@templates/content-container/content-container.component';
+
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ContentContainerComponent],
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
