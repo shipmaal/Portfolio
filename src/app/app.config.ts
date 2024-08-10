0a5005173f972f02f6fcb75fd23c481db38f3128
shipmaal <100368040+shipmaal@users.noreply.github.com>
2024-08-10 11:47:12 -0500

    fixed all of my problems

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,25 +2,21 @@ import { ApplicationConfig } from "@angular/core";
 import { provideRouter } from "@angular/router";
 import { APP_ROUTES } from "./app.router";
 import { provideClientHydration } from "@angular/platform-browser";
-import { HighlightOptions, HIGHLIGHT_OPTIONS } from "ngx-highlightjs";
+import { provideHighlightOptions } from "ngx-highlightjs";
 
 export const appConfig: ApplicationConfig = {
     providers: [
         provideRouter(APP_ROUTES),
         provideClientHydration(),
-        {
-            provide: HIGHLIGHT_OPTIONS,
-            useValue: <HighlightOptions>{
-                lineNumbers: true,
-                coreLibraryLoader: () => import('highlight.js/lib/core'),
-                lineNumbersLoader: () => import('ngx-highlightjs/line-numbers'),
-                languages: {
-                    typescript: () => import('highlight.js/lib/languages/typescript'),
-                    css: () => import('highlight.js/lib/languages/css'),
-                    xml: () => import('highlight.js/lib/languages/xml'),
-                },
-            },
-        },
+        provideHighlightOptions({
+            coreLibraryLoader: () => import('highlight.js/lib/core'),
+            lineNumbersLoader: () => import('ngx-highlightjs/line-numbers'), // Optional, add line numbers if needed
+            languages: {
+              typescript: () => import('highlight.js/lib/languages/typescript'),
+              css: () => import('highlight.js/lib/languages/css'),
+              xml: () => import('highlight.js/lib/languages/xml')
+            }
+          })
     ]
 };
 
